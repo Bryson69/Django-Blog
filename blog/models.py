232a33a83bcd06a03586eb444d0703321c3afe4f
Bryson69 <brysonmundia@gmail.com>
2021-05-27 23:17:50 +0300

    Remove category choices, Fix category model

--- blog/models.py
+++ blog/models.py
@@ -6,14 +6,9 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Post(models.Model):
-    CATEGORIES_CHOICES = (
-        ('lifestyle', 'lifestyle'),
-        ('travel', 'travel'),
-        ('fashion', 'fashion'),
-    )
     title = models.CharField(max_length=200, null=True)
+    category = models.ForeignKey('Category', null=True, on_delete=models.SET_NULL)
     content = models.TextField()
-    # category
     # image
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=CASCADE)
@@ -25,12 +20,8 @@ class Post(models.Model):
     def __unicode__(self):
         return 
 
-class Categories(models.Model):
-    title = models.CharField(max_length=200, null=True)
-    lifestyle = models.CharField(max_length=150, null=True)
-    travel = models.CharField(max_length = 150, null=True)
-    fashion = models.CharField(max_length = 150, null=True)
-
+class Category(models.Model):
+    categories = models.CharField(max_length=200, null=True)
     def __str__(self):
         return 
 
