a07f9a3b6190d60878f2f7ce586849cc955d3a07
Bryson69 <brysonmundia@gmail.com>
2021-05-28 22:20:15 +0300

    Add slug field, tag field and tag mmodels

--- blog/models.py
+++ blog/models.py
@@ -8,10 +8,14 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     title = models.CharField(max_length=200, null=True)
     category = models.ForeignKey('Category', null=True, on_delete=models.SET_NULL)
+    slug = models.SlugField(max_length=255, unique=True)
     content = models.TextField()
     # image
     date_posted = models.DateTimeField(default=timezone.now)
+    published = models.BooleanField(default=False)
+
     author = models.ForeignKey(User, on_delete=CASCADE)
+    tags = models.ManyToManyField('Tag', blank=True)
 
     # dunder str method
     def __str__(self):
@@ -19,6 +23,13 @@ class Post(models.Model):
 
     def __unicode__(self):
         return 
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
 
 class Category(models.Model):
     categories = models.CharField(max_length=200, null=True)
