cbeb95a1660974710bd07c5041adc2514f3efba9
Bryson69 <brysonmundia@gmail.com>
2021-05-30 20:22:46 +0300

    Import python classes

--- blog/models.py
+++ blog/models.py
@@ -1,16 +1,22 @@
 from django.db import models
 from django.db.models.deletion import CASCADE
 
+from cloudinary.models import CloudinaryField
+
+from django.db.models.signals import pre_save
+from django_blog.utils import unique_slug_generator
+
 from django.utils import timezone
 from django.contrib.auth.models import User
 # Create your models here.
 
 class Post(models.Model):
     title = models.CharField(max_length=200, null=True)
+    subtitle = models.CharField(max_length=255, null=True)
     category = models.ForeignKey('Category', null=True, on_delete=models.SET_NULL)
-    slug = models.SlugField(max_length=255, unique=True)
+    slug = models.SlugField(max_length=255, unique=True, null=True, blank=True)
     content = models.TextField()
-    # image
+    image = CloudinaryField(default = 'default.jpg')
     date_posted = models.DateTimeField(default=timezone.now)
     published = models.BooleanField(default=False)
 
@@ -24,6 +30,12 @@ class Post(models.Model):
     def __unicode__(self):
         return 
 
+def slug_generator(sender, instance, *args, **kwargs ):
+    if not instance.slug:
+        instance.slug = unique_slug_generator(instance)
+
+pre_save.connect(slug_generator, sender=Post)
+
 
 class Tag(models.Model):
     name = models.CharField(max_length=50, unique=True)
@@ -32,9 +44,9 @@ class Tag(models.Model):
         return self.name
 
 class Category(models.Model):
-    categories = models.CharField(max_length=200, null=True)
+    name = models.CharField(max_length=200, null=True)
     def __str__(self):
-        return 
+        return self.name
 
     def __unicode__(self):
         return 
