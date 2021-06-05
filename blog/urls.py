06a7cecbb8879ea729b7ed2e9ccd6a90932c6ac7
Bryson69 <brysonmundia@gmail.com>
2021-06-05 20:53:50 +0300

    Update urls

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.home, name='blog-home'),
+    path('post/', views.single_post, name='single-blog'),
     path('about/', views.about, name='about-page'),
+
+    # registration    
 ]
