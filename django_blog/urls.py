c127a7b70b775e208cd400973c3da4d9cc6313bf
Bryson69 <brysonmundia@gmail.com>
2021-06-07 22:31:56 +0300

    Include users/urls into project urls

--- django_blog/urls.py
+++ django_blog/urls.py
@@ -20,7 +20,9 @@ from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
     path('', include('blog.urls')),
-    path('register/', user_views.register, name='register'),
-    path('login/', user_views.login, name='login'),
+
+    path('authentication/', include('users.urls')),
+    
 ]
