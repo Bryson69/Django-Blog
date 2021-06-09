8ca82ad5da914c3023f321e8c560e7a577e8a0d2
Bryson69 <brysonmundia@gmail.com>
2021-06-10 00:27:49 +0300

    Import ListView & add single_post function to display single posts

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,24 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render, redirect
+
+from django.views.generic import ListView
 
 from .models import *
 # Create your views here.
 
 #
 def home(request):
+
     context = { 'posts' : Post.objects.all()}
+
     return render(request, 'blog/home.html', context)
 
 
-def about(request):
-    return render(request, 'blog/about.html', {'title': 'About'})
+def single_post(request, pk):
+
+    single_post = Post.objects.get(id = pk)
+
+    context = {'single_post': single_post}
+
+    return render(request, 'blog/single.html', context)
+
