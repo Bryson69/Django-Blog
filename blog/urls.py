d9dcfdbfdda4cf73b578d76884c1398af6991f4f
Bryson69 <brysonmundia@gmail.com>
2021-06-10 00:31:24 +0300

    Add paths to display single categories and single posts

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,11 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='blog-home'),
-    path('post/', views.single_post, name='single-blog'),
+    
+    path('post/<str:pk>/', views.single_post, name='post'),
+
+    path('category/<category>/', views.CatListView.as_view(), name='category'),
+    # on post id -->  path('post/<slug:slug:text>'),
     path('about/', views.about, name='about-page'),
 
     # registration    
