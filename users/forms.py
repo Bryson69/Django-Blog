71a65dfcb7c69037a5dd0c2491befe963b042398
Bryson69 <brysonmundia@gmail.com>
2021-06-10 19:13:08 +0300

    Add forms.py & Add UserLoginForm

--- users/forms.py
+++ users/forms.py
@@ -2,7 +2,10 @@ from django import forms
 
 from django.contrib.auth.models import User
 
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
+# https://docs.djangoproject.com/en/3.0/topics/auth/default/
+# https://docs.djangoproject.com/en/3.0/topics/forms/
 
 
 
@@ -13,3 +16,18 @@ class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs=
+    {
+        'class': 'form-control mb-3', 
+        'placeholder': 'Username', 
+        'id': 'login-username'
+    }))
+    password = forms.CharField(widget=forms.PasswordInput(attrs=
+    {
+        'class': 'form-control',
+        'placeholder': 'Password',
+        'id': 'login-pwd',
+    }))
