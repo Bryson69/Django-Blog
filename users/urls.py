f9868544799c86a0e060aade3543324930a57a31
Bryson69 <brysonmundia@gmail.com>
2021-06-11 22:15:18 +0300

    Import froms, views & Add paths for login & register templates

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,17 @@
-from django.contrib import admin
 from django.urls import path, include
+
+from django.contrib.auth import views as auth_views
 
 from . import views
 
+from .forms import UserLoginForm
+
+app_name = 'accounts'
+
 urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html', authentication_form=UserLoginForm), name='login'),
 
-    path('register/', views.register, name='register'),
+    path('register/', views.registerPage, name='register'),
 
-    path('login/', views.login, name='login'),
+    path('profile/', views.profilePage, name='profile'),
 ]
