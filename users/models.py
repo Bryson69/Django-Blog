4b7a05b8dd9fdb94bc3987baa70d3c1ce044ae0f
Bryson69 <brysonmundia@gmail.com>
2021-06-18 19:28:47 +0300

    import CASCADE, User, Post, CLoudunary and create profile model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+from django.db.models.fields.files import ImageField
+from django.db.models.deletion import CASCADE
+
+from cloudinary.models import CloudinaryField
+
+from blog.models import Post
+
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.CharField(max_length=100, null=True, blank=True)
+    banner = CloudinaryField(default = 'banner_default.jpg', folder = 'blog/banners/' )
+    profile_pic = CloudinaryField(default = 'default.jpg', folder = 'blog/profile_pic/' ) 
+    
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
