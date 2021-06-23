841220460908ef45065e6c73182f72f3c2a23439
Bryson69 <brysonmundia@gmail.com>
2021-06-23 17:01:31 +0300

    Add imports and url patterns

--- django_blog/urls.py
+++ django_blog/urls.py
@@ -16,13 +16,22 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.contrib.auth import views as auth_views
+
 from users import views as user_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     
     path('', include('blog.urls')),
 
-    path('authentication/', include('users.urls')),
-    
+    path('account/', include('users.urls', namespace='accounts')),
+
+    path('account/', include('django.contrib.auth.urls')),
+
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
