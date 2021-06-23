f688435d004c070ea59cc6922df032bbab738adf
Bryson69 <brysonmundia@gmail.com>
2021-06-23 17:13:01 +0300

    Add url patterms

--- blog/urls.py
+++ blog/urls.py
@@ -1,16 +1,27 @@
 from django.urls import path
-from .views import PostListView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+
 from . import views
 
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
     
-    path('post/<str:pk>/', views.single_post, name='post'),
+    path('post/<str:pk>/', PostDetailView.as_view(), name='post-detail'),
+
+    path('post-form/', PostCreateView.as_view(), name='post-create'), 
+
+    path('post/<str:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+
+    path('post/<str:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 
     path('category/<category>/', views.CatListView.as_view(), name='category'),
+
     # on post id -->  path('post/<slug:slug:text>'),
     path('about/', views.about, name='about-page'),
+
+    path('project-overview/', views.projectOverview, name='project-overview'),
+
 
     # registration    
 ]
