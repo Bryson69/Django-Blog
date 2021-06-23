62e59c051cca02f6c51293e842885289ab964346
Bryson69 <brysonmundia@gmail.com>
2021-06-23 17:04:55 +0300

    Add class to update a post

--- blog/views.py
+++ blog/views.py
@@ -50,6 +50,24 @@ class PostCreateView(LoginRequiredMixin, CreateView):
     def get_success_url(self):
         return reverse_lazy('blog-home')
     
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title','location','category','content','image']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+    def get_success_url(self):
+        return reverse_lazy('blog-home')
+    
     
 
 class CatListView(ListView):
