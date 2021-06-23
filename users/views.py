d3a89727e2b36c9467c5760feba045b067e75e99
Bryson69 <brysonmundia@gmail.com>
2021-06-23 16:50:46 +0300

    Add Registration view

--- users/views.py
+++ users/views.py
@@ -1,24 +1,31 @@
+from django.http.response import HttpResponseForbidden
 from django.shortcuts import redirect, render
 
 from django.contrib.auth.forms import UserCreationForm
-
-from .forms import UserRegistrationForm
-
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+from django.utils.encoding import force_bytes, force_text
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from .token import account_activation_token
+from django.conf import settings
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from .forms import UserRegisterForm, AuthenticationForm, UserUpdateForm, ProfileUpdateForm
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-# Create your views here.
 
-def register(request):
+def registerPage(request):
     if request.method == 'POST':
-        form  = UserRegistrationForm(request.POST)
-
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account Created for {username}!')
-            return redirect ('login')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'registration/register.html', {'form': form})
 
-    form  = UserRegistrationForm()
-    return render(request, 'auth/register.html',  {'form': form} )
+# def loginPage(request):
+#     return render(request, 'registration/login.html')
 
-def login(request):
-    return render(request, 'auth/login.html')
