af5f4eeea92cd5c93d30a5be38b9ba2b44f84fcd
Bryson69 <brysonmundia@gmail.com>
2021-06-23 16:58:21 +0300

    Add registration and profile forms

--- users/forms.py
+++ users/forms.py
@@ -1,21 +1,66 @@
 from django import forms
 
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-
+from django.core.exceptions import ValidationError
+from django.contrib.auth.forms import AuthenticationForm, PasswordResetForm, PasswordChangeForm, SetPasswordForm
+from .models import Profile
 # https://docs.djangoproject.com/en/3.0/topics/auth/default/
 # https://docs.djangoproject.com/en/3.0/topics/forms/
 
-
-
-class UserRegistrationForm(UserCreationForm):
-
+class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+# class RegistrationForm(forms.ModelForm):
+
+#     username = forms.CharField(
+#         label='Enter Username', min_length=4, max_length=50, help_text='Required')
+#     email = forms.EmailField(max_length=100, help_text='Required', error_messages={
+#         'required': 'Sorry, you will need an email'})
+#     password = forms.CharField(label='Password', widget=forms.PasswordInput)
+#     password2 = forms.CharField(
+#         label='Repeat password', widget=forms.PasswordInput)
+
+#     class Meta:
+#         model = User
+#         fields = ('username', 'email', 'first_name',)
+
+#     def clean_username(self):
+#         username = self.cleaned_data['username'].lower()
+#         r = User.objects.filter(username=username)
+#         if r.count():
+#             raise ValidationError("Username already exists")
+#         return username
+
+#     def clean_password2(self):
+#         pass_check = self.cleaned_data
+#         if pass_check['password'] != pass_check['password2']:
+#             raise forms.ValidationError('Passwords do not match.')
+#         return pass_check['password2']
+
+#     def clean_email(self):
+#         email = self.cleaned_data['email']
+#         if User.objects.filter(email=email).exists():
+#             raise forms.ValidationError(
+#                 'Please use another Email, that is already taken')
+#         return email
+
+#     def __init__(self, *args, **kwargs):
+#         super().__init__(*args, **kwargs)
+#         self.fields['username'].widget.attrs.update(
+#             {'class': 'form-control mb-3', 'placeholder': 'Username'})
+#         self.fields['email'].widget.attrs.update(
+#             {'class': 'form-control mb-3', 'placeholder': 'E-mail', 'name': 'email', 'id': 'id_email'})
+#         self.fields['password'].widget.attrs.update(
+#             {'class': 'form-control', 'placeholder': 'Password'})
+#         self.fields['password2'].widget.attrs.update(
+#             {'class': 'form-control', 'placeholder': 'Repeat Password'})
+
 
 
 class UserLoginForm(AuthenticationForm):
@@ -31,3 +76,16 @@ class UserLoginForm(AuthenticationForm):
         'placeholder': 'Password',
         'id': 'login-pwd',
     }))
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', ]
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['location', 'bio', 'banner', 'profile_pic']
