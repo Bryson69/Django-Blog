d5cba9765b16e0e0c82f675f0355cda88df6b635
Bryson69 <brysonmundia@gmail.com>
2021-07-02 22:33:45 +0300

    Add bookmark urls

--- users/urls.py
+++ users/urls.py
@@ -12,10 +12,11 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html', authentication_form=UserLoginForm), name='login'),
 
     path('logout/', views.logout, name='logout'),
-
-
     path('register/', views.registerPage, name='register'),
 
     path('profile/', views.profilePage, name='profile'),
     path('update-profile/', views.updateProfile, name='update-profile'),
+
+    path('bookmark/<str:id>/', views.bookmark_add, name='bookmark_add'),
+    path('bookmarks/', views.bookmark_list, name='bookmark_list'),
 ]
