4c84ae4b5749007a39735046f58a5806b1974a73
Bryson69 <brysonmundia@gmail.com>
2021-07-13 20:25:06 +0300

    Add comment model and update post model

--- blog/models.py
+++ blog/models.py
@@ -9,7 +9,12 @@ from django_blog.utils import unique_slug_generator
 from django.urls import reverse
 from django.utils import timezone
 from django.contrib.auth.models import User
+from mptt.models import MPTTModel, TreeForeignKey
 # Create your models here.
+
+class BookmarkManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset() .filter()
 
 class Post(models.Model):
     title = models.CharField(max_length=200, null=True)
@@ -18,12 +23,20 @@ class Post(models.Model):
     slug = models.SlugField(max_length=255, unique=True, null=True, blank=True)
     content = models.TextField()
     image = CloudinaryField(default = 'default.jpg')
+    # image = models.ImageField(upload_to='blog_pics',blank=True)
+    image_caption = models.CharField(max_length = 100, default = 'Photo by Blog')
+    
     date_posted = models.DateTimeField(default=timezone.now)
     published = models.BooleanField(default=False)
+    favourites = models.ManyToManyField(User, related_name='favourite', default=None, blank=True)
+    likes = models.ManyToManyField(User, related_name='likes', default=None, blank=True)
+    like_count = models.BigIntegerField(default='0')
+
 
     author = models.ForeignKey(User, on_delete=CASCADE)
     tags = models.ManyToManyField('Tag', blank=True)
-
+    objects = models.Manager()  # default manager
+    bookmarkmanager = BookmarkManager()  # custom manager
     # dunder str method
     def __str__(self):
         return self.title
@@ -37,7 +50,14 @@ class Category(models.Model):
     def __unicode__(self):
         return 
 
+class Comment(models.Model):
+    name = models.CharField(max_length=255, default=None )
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,default=None, related_name='comments')
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
 
+    def __str__(self):
+        return '%s - %s' % (self.post.title, self.name)
 class Tag(models.Model):
     name = models.CharField(max_length=50, unique=True)
 
